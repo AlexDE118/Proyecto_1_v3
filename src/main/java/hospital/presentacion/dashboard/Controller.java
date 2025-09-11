b0be8b486679f267309d8f77f09a28324a91c038
IsaacEspAl1 <isaac.espinoza.alvarado@est.una.ac.cr>
2025-09-11 14:13:13 -0600

    Despacho y prescripciones

---
+++ src/main/java/hospital/presentacion/dashboard/Controller.java
@@ -0,0 +1,36 @@
+package hospital.presentacion.dashboard;
+
+import hospital.logic.Prescripcion;
+import hospital.logic.Service;
+
+import java.time.LocalDate;
+import java.util.List;
+
+public class Controller {
+    private Model model;
+    private View2 view;
+
+    public Controller(Model model, View2 view) {
+        this.model = model;
+        this.view = view;
+        this.view.setController(this);
+        this.view.setModel(model);
+    }
+
+//    public void refresh() {
+////        model.loadData();
+//        view.update();
+//    }
+
+    public void cargarPrescripciones(LocalDate desde, LocalDate hasta, String medicamento) {
+        List<Prescripcion> result = Service.instance().getPrescripciones(desde, hasta, medicamento);
+
+        System.out.println("Resultado tamaño: " + result.size());
+        for (Prescripcion p : result) {
+            System.out.println(p.getReceta() + " - " + p.getFechaRetiro());
+        }
+        model.setListaPrescripcion(result);   // ✅ en lugar de setCurrent
+    }
+
+
+}
